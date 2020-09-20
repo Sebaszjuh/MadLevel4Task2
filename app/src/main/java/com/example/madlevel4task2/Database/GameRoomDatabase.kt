9c19f0034d018a9e5994cd42abb103786c405a2b
griends1 <Steelpan1>
2020-09-20 14:33:35 +0200

    implemented dateconverter & database + new strings

--- app/src/main/java/com/example/madlevel4task2/Database/GameRoomDatabase.kt
+++ app/src/main/java/com/example/madlevel4task2/Database/GameRoomDatabase.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.madlevel4task2.DAO.GameDAO
 import com.example.madlevel4task2.Model.Game
 
 
 // Database van product class
 @Database(entities = [Game::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class GameRoomDatabase : RoomDatabase(){
 
     // instantieren van DAO
