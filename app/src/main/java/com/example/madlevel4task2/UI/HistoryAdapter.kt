dacaba77c8347a7c2806e0ee05802950a7116568
griends1 <Steelpan1>
2020-09-20 22:48:09 +0200

    Implemented history fragment && adapter

---
+++ app/src/main/java/com/example/madlevel4task2/UI/HistoryAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.madlevel4task2.UI
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel4task2.Model.Game
+import com.example.madlevel4task2.Model.Game.Companion.possibleMoves
+import com.example.madlevel4task2.R
+import kotlinx.android.synthetic.main.item_history.view.*
+
+class HistoryAdapter(private val gameHistory: List<Game>) :
+    RecyclerView.Adapter<HistoryAdapter.ViewHolder>() {
+
+    private lateinit var context: Context
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_history, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int = gameHistory.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) =
+        holder.bind(gameHistory[position])
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(game: Game) {
+            itemView.itemResult.text = parseResult(game.gameResult)
+            itemView.itemDate.text = game.playDate.toString()
+            itemView.playerImage.setImageResource(possibleMoves[game.chosenAction])
+            itemView.computerImage.setImageResource(possibleMoves[game.computerAction])
+        }
+
+        private fun parseResult(gameResult: Int): String{
+            if(gameResult == 0){
+                return "Tie"
+            }
+            if(gameResult == 1){
+                return "You win!"
+            }
+            return "You Lose"
+        }
+    }
+
+}
--- app/src/main/java/com/example/madlevel4task2/UI/HistoryFragment.kt
+++ app/src/main/java/com/example/madlevel4task2/UI/HistoryFragment.kt
@@ -1,4 +1,89 @@
 package com.example.madlevel4task2.UI
 
-class HistoryFragment {
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel4task2.Model.Game
+import com.example.madlevel4task2.R
+import com.example.madlevel4task2.Repository.GameRepository
+import kotlinx.android.synthetic.main.fragment_history.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class HistoryFragment : Fragment() {
+
+    private val gameHistory = arrayListOf<Game>()
+    private val historyAdapter = HistoryAdapter(gameHistory)
+    private lateinit var gameRepository: GameRepository
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
+        return inflater.inflate(R.layout.fragment_history, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        gameRepository = GameRepository(requireContext())
+        initViews()
+    }
+
+    private fun initViews() {
+        listHistory.layoutManager =
+            LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+        listHistory.adapter = historyAdapter
+        listHistory.addItemDecoration(
+            DividerItemDecoration(
+                requireContext(),
+                DividerItemDecoration.VERTICAL
+            )
+        )
+        getGameHistoryFromDatabase()
+    }
+
+    private fun getGameHistoryFromDatabase() {
+        CoroutineScope(Dispatchers.Main).launch {
+            val gameHistory = withContext(Dispatchers.IO) {
+                gameRepository.getAllGames()
+            }
+            this@HistoryFragment.gameHistory.clear()
+            this@HistoryFragment.gameHistory.addAll(gameHistory)
+            this@HistoryFragment.historyAdapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun clearHistory() {
+        CoroutineScope(Dispatchers.Main).launch {
+            withContext(Dispatchers.IO) {
+                gameRepository.deleteAllGames()
+            }
+            getGameHistoryFromDatabase()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_history, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_delete -> {
+                clearHistory()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
